2d965027dca2557067d3d0c88fc9077d9a4cbc07
Henry Abramovich <henry.abramovich@gmail.com>
2024-04-05 02:33:04 -0400

    spelling error lol

--- frontend/src/components/Content/Group/Group.jsx
+++ frontend/src/components/Content/Group/Group.jsx
@@ -7,7 +7,7 @@ export default function Group() {
         <div className='group'>
             <Member memberName={"Henry Abramovich"} memberDescription={"i am so great. i am simply the best."}/>
             <Member memberName={"Jasraj Singh"} memberDescription={"wow i cant believe its not butter"}/>
-            <Member memberName={"Jay Sharma"} memberDescription={"youre telling me a shrimp friend this rice? are ya at least paying him a living wage.."}/>
+            <Member memberName={"Jay Sharma"} memberDescription={"youre telling me a shrimp fried this rice? are ya at least paying him a living wage.."}/>
             <Member memberName={"Sana Sharma"} memberDescription={"the voices the freaking voicessssss. anyways, word to yo mama"}/>
             <Member memberName={"Hiva Sharma"} memberDescription={""}/>
             <Member memberName={"TonyJin Sharma"} memberDescription={""}/>
