41c770f570cf987221c5a75c88f8a44aa53ef86d
henry <henry.abramovich@gmail.com>
2024-04-06 19:50:19 -0400

    webm

--- frontend/src/components/Body/Content/Podcast/Podcast.jsx
+++ frontend/src/components/Body/Content/Podcast/Podcast.jsx
@@ -32,7 +32,7 @@ export default function Podcast() {
         </div>
         <video controls className='video' key={pState.title}>
           <source src={pState.src_mp4} type='video/mp4' />
-          <source src={pState.src_webm} type='video/' />
+          <source src={pState.src_webm} type='video/webm' />
         </video>
         <div className='img-cont' onClick={() => handlePI(1)}>
           <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipart-library.com%2Fimages_k%2Farrow-icon-png-transparent%2Farrow-icon-png-transparent-4.png&f=1&nofb=1&ipt=f52419e1d47898642e2af6746ce63b9e4d529ea0ea497cc26d952f4e468362fe&ipo=images" alt="" />
