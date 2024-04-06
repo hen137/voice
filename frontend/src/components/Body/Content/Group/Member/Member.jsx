3247e10bdc62f3fd9c74f228dafd4a4afa5aa086
henry <henry.abramovich@gmail.com>
2024-04-06 19:30:01 -0400

    env delted

--- frontend/src/components/Body/Content/Group/Member/Member.jsx
+++ frontend/src/components/Body/Content/Group/Member/Member.jsx
@@ -3,7 +3,7 @@ import './Member.css';
 export default function Group({ memberName, memberDescription }) {
     return (
         <div className="member">
-            <img className='headshot' src={"/headshots/" + memberName + ".jpg"} alt={'/headshots/squid.jpg'} />
+            <img className='headshot' src={"/headshots/" + memberName + ".jpg"} alt={"/headshots/squid.jpg"} />
             <h3 className='margin font-SCP'>{memberName}</h3>
             <p className='margin font-SCP'>{memberDescription}</p>
         </div>
