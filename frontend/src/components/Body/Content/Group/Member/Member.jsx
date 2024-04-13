095542e21864d3184f7447d8cb7ad7712dfca399
Henry Abramovich <henry.abramovich@gmail.com>
2024-04-13 00:35:26 -0400

    migrated media to public s3 links

--- frontend/src/components/Body/Content/Group/Member/Member.jsx
+++ frontend/src/components/Body/Content/Group/Member/Member.jsx
@@ -1,9 +1,12 @@
 import './Member.css';
 
 export default function Group({ memberName, memberDescription }) {
+    const firstName = memberName.split(" ")[0];
+    const lastName = memberName.split(" ")[1];
+    
     return (
         <div className="member">
-            <img className='headshot' src={"/headshots/" + memberName + ".jpg"} alt={"/headshots/squid.jpg"} />
+            <img className='headshot' src={"https://mindfulconsumer-media.s3.us-east-2.amazonaws.com/headshots/" + firstName + "+" + lastName + ".jpg"} alt="" />
             <h3 className='margin font-SCP'>{memberName}</h3>
             <p className='margin font-SCP'>{memberDescription}</p>
         </div>
--- frontend/src/components/Body/Content/Podcast/Podcast.jsx
+++ frontend/src/components/Body/Content/Podcast/Podcast.jsx
@@ -3,9 +3,9 @@ import './Podcast.css'
 
 export default function Podcast() {
   const state = [
-    { title: 'Episode 1: The Nitty Gritties of Consumerism', src_mp4: '/episodes/Mindful_Consumer_Podcast_Ep1video.mp4', src_webm: '/episodes/Mindful_Consumer_Podcast_Ep1.webm' },
-    { title: 'Episode 2: A Techno-social perspective', src_mp4: '/episodes/Mindful_Consumer_Podcast_Ep2video.mp4', src_webm: '/episodes/Mindful_Consumer_Podcast_Ep2.webm'  },
-    { title: 'Episode 3: Digital Detox', src_mp4: '/episodes/Mindful_Consumer_Podcast_Ep3video.mp4', src_webm: '/episodes/Mindful_Consumer_Podcast_Ep3.webm'  }
+    { title: 'Episode 1: The Nitty Gritties of Consumerism', src_mp4: 'https://mindfulconsumer-media.s3.us-east-2.amazonaws.com/episodes/Mindful_Consumer_Podcast_Ep1video.mp4', src_webm: 'https://mindfulconsumer-media.s3.us-east-2.amazonaws.com/episodes/Mindful_Consumer_Podcast_Ep1.webm' },
+    { title: 'Episode 2: A Techno-social perspective', src_mp4: 'https://mindfulconsumer-media.s3.us-east-2.amazonaws.com/episodes/Mindful_Consumer_Podcast_Ep2video.mp4', src_webm: 'https://mindfulconsumer-media.s3.us-east-2.amazonaws.com/episodes/Mindful_Consumer_Podcast_Ep2.webm' },
+    { title: 'Episode 3: Digital Detox', src_mp4: 'https://mindfulconsumer-media.s3.us-east-2.amazonaws.com/episodes/Mindful_Consumer_Podcast_Ep3video.mp4', src_webm: 'https://mindfulconsumer-media.s3.us-east-2.amazonaws.com/episodes/Mindful_Consumer_Podcast_Ep3.webm' }
   ]
 
   const [pIndex, setPIndex] = useState(0);
@@ -32,7 +32,7 @@ export default function Podcast() {
         </div>
         <video controls className='video' key={pState.title}>
           <source src={pState.src_mp4} type='video/mp4' />
-          <source src={pState.src_webm} type='video/webm' />
+          <source src={pState.src_webm} type='video/' />
         </video>
         <div className='img-cont' onClick={() => handlePI(1)}>
           <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipart-library.com%2Fimages_k%2Farrow-icon-png-transparent%2Farrow-icon-png-transparent-4.png&f=1&nofb=1&ipt=f52419e1d47898642e2af6746ce63b9e4d529ea0ea497cc26d952f4e468362fe&ipo=images" alt="" />
